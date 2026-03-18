192f9c4d5fb130e6db89d3e52525b10b423e1821
brandon-nam <brandonnam2020@gmail.com>
2026-03-18 20:37:42 +0800

    e2e: add tests for product details and about page

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run dev',
     url: 'http://localhost:3000',
-    reuseExistingServer: false,
+    reuseExistingServer: true,
     timeout: 180 * 1000,
     env: {
        NODE_ENV: 'test',
